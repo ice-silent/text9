d0a3d5fd7b2814329572f859413014043b1a01e7
ice-silent <fumingzhen8@163.com>
2016-09-07 10:21:45 +0800

    掌握在应用程序中生成多个Activity，学会在Activity之间跳转.

---
+++ app/src/main/java/com/example/fumingzhen/text9/AnotherActivity.java
@@ -0,0 +1,29 @@
+package com.example.fumingzhen.text9;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear);
+        Button b2=(Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(AnotherActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+}
